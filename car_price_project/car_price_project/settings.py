2a441c68a5b6cc94d43a874c3664c905d8a8bb02
sarath-sai <your_actual_github_sarathsaikumarkattunga1@gmail.com>
2026-09-20 18:25:46 +0530

    Use environment secret key for production

--- car_price_project/car_price_project/settings.py
+++ car_price_project/car_price_project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j&yw)_w1)(_(2ab@_49m9h%sppqe0jm_vczoykf_3ea#(*0%+r'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
