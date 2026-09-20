bd2af86bb8de64e590d25e638b47891c644e1919
sarath-sai <your_actual_github_sarathsaikumarkattunga1@gmail.com>
2026-09-20 17:57:53 +0530

    Prepare Django app for deployment

--- car_price_project/car_price_project/settings.py
+++ car_price_project/car_price_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!5umpy*s&wwa44l^gh_z_e^usk)*h4!7*^ej=z92hvg7$_@+gg'
+SECRET_KEY = 'j&yw)_w1)(_(2ab@_49m9h%sppqe0jm_vczoykf_3ea#(*0%+r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
